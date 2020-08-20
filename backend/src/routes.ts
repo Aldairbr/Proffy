0724740309797bcf46d585619840cfaaac75a7bb
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-08-20 06:13:34 -0300

    refact/ changed routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,9 @@
 import express from 'express'
+import classesController from './controllers/classesController'
 
 const routes = express.Router()
 
-routes.get('/users', (request, response) => {
-  return response.json({message: 'Ta funfando'})
-})
+routes.post('/classes', classesController.Store)
+routes.get('/classes', classesController.Index)
 
 export default routes
