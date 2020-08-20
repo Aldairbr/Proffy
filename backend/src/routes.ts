8c91396c4d04dc2a11e4b2e60b70e136559efff6
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-08-20 20:50:58 -0300

    feat/ added new route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import classesController from './controllers/classesController'
+import connetionsController from './controllers/connectionsController'
 
 const routes = express.Router()
 
+
 routes.post('/classes', classesController.Store)
 routes.get('/classes', classesController.Index)
+
+routes.post('/connections', connetionsController.Store)
+routes.get('/connections', connetionsController.Index)
+
+
 
 export default routes
